cd0f9a081f8e8d55f223ad85a30f67086986284e
Mikhail Malygin <mihail.malygin@gmail.com>
2026-07-09 09:55:38 +0500

    fix remote launch tests

--- src/test/java/config/AuthConfig.java
+++ src/test/java/config/AuthConfig.java
@@ -2,7 +2,10 @@ package config;
 
 import org.aeonbits.owner.Config;
 
+@Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
+        "system:properties",
+        "system:env",
         "classpath:ui.properties"
 })
 
